18a4752eebda9e8433e2789bc6178c7a14b3426a
Pauline123456789 <117153576+Pauline123456789@users.noreply.github.com>
2023-09-29 19:36:30 +0700

    Update Class1.cs

--- 2.1/Class1.cs
+++ 2.1/Class1.cs
@@ -9,12 +9,14 @@ namespace Student_
     class Student
     {
         public string familia, data;
-        public int nomer;
+        public int number
+;
         public int[] progress;
 
-        public void writeInConsoleInfo(string surname, string data, int nomer, int[] progress)
+        public void writeInConsoleInfo(string surname, string data, int number
+, int[] progress)
         {
-            Console.WriteLine("Фамилия: {0}\nДень рождение: {1}\nГруппа: {2}\n", surname, data, nomer);
+            Console.WriteLine("Фамилия: {0}\nДень рождение: {1}\nГруппа: {2}\n", surname, data, number);
             Console.WriteLine("Успеваемость:");
             foreach (int i in progress)
             {
